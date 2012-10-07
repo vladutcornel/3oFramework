3d610105873e8c00d2ca1d64a48fa87ed7b0bee9
Cornel Borina <cornel@scoalaweb.com>
2012-10-07 18:06:31 +0200

    Added support for user "whereis"
    Now, using trio_whereis(), the framework user can register his/her own
    class files to be loaded on demand

--- 3o/whereis.php
+++ 3o/whereis.php
@@ -4,6 +4,16 @@
  * @package 3oScript
  * @author Cornel Borina <cornel@scoalaweb.com>
  */
+
+/**
+ * User defined classes
+ */
+global $WHEREIS_USER;
+$WHEREIS_USER = array();
+
+/**
+ * TriO classes
+ */
 $WHEREIS = array (
   'CSSAtribute' => '/css/CSSAtribute.php',
   'CSSColor' => '/css/CSSColor.php',
@@ -36,12 +46,51 @@ $WHEREIS = array (
 
 function trio_autoload($class_name){
     global $WHEREIS;
+    global $WHEREIS_USER;
+    // try to load TriO script class
     if (isset($WHEREIS[$class_name]))
     {
         include TRIO_DIR.'/'.$WHEREIS[$class_name];
     }
+    
+    // try to load User-defined class
+    if (isset($WHEREIS_USER[$class_name]))
+    {
+        include $WHEREIS_USER[$class_name];
+    }
 }
+
 /*
  * Register autoload function and set it to prepand (3rd param) so other autoload functions can be declared
  */
 spl_autoload_register ('trio_autoload', true, true);
+
+/**
+ * Tell the script where to look for the invoked class
+ * You can provide a parameters list with the odd index (1st param, 3rd...) 
+ * being the class names andd the even parameters being the file path.
+ * Or you can directly provide an associative array with the keys being the 
+ * class names and the alues the path
+ * @param array $whereis array(class_name=>file_path)
+ */
+function trio_whereis()
+{
+    $first_arg = func_get_arg(0);
+    if (!is_array($first_arg))
+    {
+        // we got a list
+        $nr_args = func_num_args();
+        $args = func_get_args();
+        $new_args = array();
+        for ($i = 1; $i < $nr_args; $i+=2)
+        {
+            $new_args[$args[$i-1]] = $args[$i];
+        }
+        trio_whereis($new_args);
+    }
+    global $WHEREIS_USER;
+    foreach($first_arg as $class=>$file)
+    {
+        $WHEREIS_USER[$class] = $file;
+    }
+}
