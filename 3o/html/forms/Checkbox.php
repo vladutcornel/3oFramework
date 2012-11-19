009adab531af22894e7b6edbaae626432c9cf07b
Cornel Borina <cornel@scoalaweb.com>
2012-11-19 21:43:01 +0100

    Properly set the field value in the constructor

--- 3o/html/forms/Checkbox.php
+++ 3o/html/forms/Checkbox.php
@@ -1,15 +1,29 @@
 <?php
 /**
- * Description of ClickableElement
+ * A HTML Checkbox.
  *
- * @author cornel
+ * @author Cornel Borina <cornel@scoalaweb.com>
+ * @package 3oLibrary
+ * @subpackage HTML
  */
 class Checkbox extends Input{
-    
+    /**
+     * @param string $name the name of the field
+     * @param mixed $value the value of the checkbox field
+     * @param string $id
+     */
     public function __construct($name, $default = 1, $id = '') {
         parent::__construct('checkbox', $name, $default, $id);
+        $this->setAttribute('value', $default);
     }
     
+    /**
+     * Marks the checkbox as checked if the provided value matches the field value
+     * Otherwise, unchecks it.
+     * To really set the value, use Checkbox::setAttribute(...)
+     * @param type $new_value
+     * @return \Checkbox
+     */
     public function setValue($new_value) {
         if ($new_value == $this->getAttribute('value'))
         {
@@ -17,8 +31,14 @@ class Checkbox extends Input{
         } else{
             $this->removeAttribute('checked');
         }
+        
+        return $this;
     }
     
+    /**
+     * Returns the value of the field if checked or false if not
+     * @return boolean|string
+     */
     public function getValue() {
         if ('' != $this->getAttribute('checked'))
         {
