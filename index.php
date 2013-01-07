8bb9be1c45ed3b37b5c562143690c6c52524f4f9
cornel <cornel@cornel-GeForce7050M-M>
2013-01-07 19:08:04 +0200

    Preparing Trio Core for first release

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Description of index
+ *
+ * @author cornel
+ */
+class index {
+    public function main(){
+?><!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>TrioFramework works</title>
+    </head>
+    <body>
+        <h1>Trio Framework is up and running</h1>
+        <p>You must have done something good</p>
+    </body>
+</html>
+<?php
+    }
+}
